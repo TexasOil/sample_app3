354d6365c37d7a8c75ec5e05f8b431d25cfa2bb5
Helium Labs <tmmyracle@gmail.com>
2013-11-24 15:58:07 -0600

    Fix security features

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'edd011616144bcc4a118fe21326404aff1e25a02dac6706669a6b8cff15ea70dced95132d6425f95f19b54d9421088e1999b0f3ddce60ae73cfca298f1bebe0b'
+
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist? (token_file)
+		#Use the existing token
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in the token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
