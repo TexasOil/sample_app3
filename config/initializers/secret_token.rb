04f9f1dd024e9b20eb8a865831f5effc9e010354
Helium Labs <tmmyracle@gmail.com>
2013-11-24 15:53:23 -0600

    Updated ignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'edd011616144bcc4a118fe21326404aff1e25a02dac6706669a6b8cff15ea70dced95132d6425f95f19b54d9421088e1999b0f3ddce60ae73cfca298f1bebe0b'
